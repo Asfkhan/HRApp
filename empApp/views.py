e2b20bf9da4450c5078e2f3fc8b85ed3d6613197
Aseef Khan <ak5488919@gmail.com>
2024-08-19 15:29:06 +0530

    Update views.py

--- empApp/views.py
+++ empApp/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import JobRole, Employee
 from .forms import JobRoleForm, EmployeeForm
+from django.db.models import Q
 
 def job_role_list(request):
     job_roles = JobRole.objects.all()
@@ -18,9 +19,36 @@ def job_role_create(request):
     return render(request, 'job_role_form.html', {'form': form})
 
 def employee_list(request):
-    employees = Employee.objects.select_related('job_role').all() 
-    return render(request, 'employee_list.html', {'employees': employees})
-
+    employees = Employee.objects.all()
+    
+    # Search query
+    search_query = request.GET.get('search', '')
+    if search_query:
+        employees = employees.filter(
+            Q(first_name__icontains=search_query) | 
+            Q(last_name__icontains=search_query) | 
+            Q(job_role__name__icontains=search_query)
+        )
+    
+    # Filter by Job Role
+    job_role_id = request.GET.get('job_role', '')
+    if job_role_id:
+        employees = employees.filter(job_role__id=job_role_id)
+    
+    # Filter by Hiring Date
+    hiring_date = request.GET.get('hiring_date', '')
+    if hiring_date:
+        employees = employees.filter(date_hired=hiring_date)
+    
+    job_roles = JobRole.objects.all()
+    
+    return render(request, 'employee_list.html', {
+        'employees': employees,
+        'job_roles': job_roles,
+        'search_query': search_query,
+        'selected_job_role': job_role_id,
+        'selected_hiring_date': hiring_date,
+    })
 
 def employee_create(request):
     if request.method == 'POST':
