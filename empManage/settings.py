548a9167f4b95ed7ec46eae7670ad6d0b81672a5
Aseef Khan <ak5488919@gmail.com>
2024-08-19 15:16:19 +0530

    Update settings.py

--- empManage/settings.py
+++ empManage/settings.py
@@ -14,8 +14,6 @@ import os
 from pathlib import Path
 
 
-LOGIN_URL = '/accounts/login/'
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
