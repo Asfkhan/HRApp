65d798aba4d8a0df25a2acfd7fa8b3e29911d14b
Aseef Khan <ak5488919@gmail.com>
2024-08-19 15:14:30 +0530

    Update urls.py

--- empManage/urls.py
+++ empManage/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path
 from empApp.views import employee_list, employee_create, job_role_list, job_role_create, employee_list, employee_create
 
