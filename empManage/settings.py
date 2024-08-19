7421a5ea1705a3cae90fb195874ae38cf68917a5
Aseef Khan <ak5488919@gmail.com>
2024-08-19 16:11:10 +0530

    Update settings.py

--- empManage/settings.py
+++ empManage/settings.py
@@ -13,7 +13,7 @@ import dj_database_url
 import os
 from pathlib import Path
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
